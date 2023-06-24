489f5ab3997d1678b415c01d6dc28058de0d611d
SyedaBareehaAli <61383929+syedabareehaali@users.noreply.github.com>
2023-06-24 22:33:41 +0500

    Updated serializers.py

--- App1/serializers.py
+++ App1/serializers.py
@@ -1,16 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from django.contrib.auth.models import User
-from django.contrib.auth import  authenticate
-from rest_framework import serializers
 
-# User Serializer
+# # User Serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email')
 
-# Register Serializer
+# # Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -19,34 +16,3 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
-    
-
-
-class AuthSerializer(serializers.Serializer):
-    '''serializer for the user authentication object'''
-   
-    username = serializers.CharField()
-    email = serializers.CharField()
-    password = serializers.CharField(
-        style={'input_type': 'password'},
-        trim_whitespace=False
-    )    
-    def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
-        email = attrs.get('email')
-        
-        user = authenticate(
-            request=self.context.get('request'),
-            username=username,
-            password=password,
-            email=email
-        )
-        
-        if not user:
-            msg = ('Unable to authenticate with provided credentials')
-            raise serializers.ValidationError(msg, code='authentication')
-
-        attrs['user'] = user
-        return 
-
